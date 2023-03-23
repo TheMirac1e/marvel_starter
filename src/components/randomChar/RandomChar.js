ca562f8c56854bc76a1998657fbf947deb3339c1
Even Koshliak <evenkoshliak@o-s-i.org>
2023-03-23 15:39:34 +0200

    JS - implemented custom hook, small refactor

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,16 +3,14 @@ import { useEffect, useState } from "react";
 import mjolnir from '../../resources/img/mjolnir.png';
 
 import './randomChar.scss';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 const RandomChar = () => {
   const [char, setChar] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
 
-  const marvelService = new MarvelService()
+  const { loading, error, getCharacter, clearError } = useMarvelService()
 
   useEffect(() => {
     updateChar();
@@ -25,26 +23,13 @@ const RandomChar = () => {
 
   const onCharLoaded = (char) => {
     setChar(char);
-    setLoading(false);
-  }
-
-  const onCharLoading = () => {
-    setLoading(true);
-    setError(false);
-  }
-
-  const onError = () => {
-    setLoading(false);
-    setError(true);
   }
 
   function updateChar() {
+    clearError();
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    onCharLoading();
-    marvelService
-      .getCharacter(id)
+    getCharacter(id)
       .then(onCharLoaded)
-      .catch(onError)
   }
 
   const errorMessage = error ? <ErrorMessage /> : null;
